6f7e3fcd9ac2a85b1521371526064b6fc57c0b78
alexn <alexn@lexansoft.com>
2016-02-03 14:53:56 -0800

    some reorganization

--- plugin/js/background.js
+++ plugin/js/background.js
@@ -3,9 +3,7 @@ httpProvider = new HttpProvider()
 Web3 = require( "web3")
 web3 = new Web3( httpProvider )
 
-var ICON_BLINKING_TIMEOUT = 200
-var current_icon_index = {} //default
-var user_action_is_needed = {}
+tabs = require( "/plugin_modules/ep_tabs.js" )
 
 function _call_content_page( tab, dataload, id ) {
       chrome.tabs.sendMessage(tab.id, 
@@ -16,50 +14,11 @@ function _call_content_page( tab, dataload, id ) {
       }, function(response) {});
 }
 
-function flipPageIcon( tab_id ) {
-    
-    current_icon_index[tab_id] = ( current_icon_index[tab_id] + 1 ) % 2
-    
-    if( current_icon_index[tab_id] == 0 ) {
-        chrome.pageAction.setIcon( {
-            tabId: tab_id,
-            path: {
-                "19": "images/ethereum_logo19.png",      
-                "38": "images/ethereum_logo38.png"           
-            }
-        })
-    }
-    else {    
-        chrome.pageAction.setIcon( {
-            tabId: tab_id,
-            path: {
-                "19": "images/ethereum_logo_red19.png",      
-                "38": "images/ethereum_logo_red38.png"           
-            }
-        })
-    }
-    
-    if( user_action_is_needed[ tab_id ] ) {
-        setTimeout( function() { flipPageIcon( tab_id ) }, ICON_BLINKING_TIMEOUT )
-    }    
-}
-
-function RequestForAction( tab_id, v ) {
-    user_action_is_needed[ tab_id ] = v
-    current_icon_index[tab_id] = 0
-    
-    if( v ) {
-        flipPageIcon( tab_id )
-    }
-}
-
-
-
 chrome.runtime.onMessage.addListener( function(message, sender, sendResponse ) {
     if (message && message.type == 'ethereum_content2bg') {
         
-        chrome.pageAction.show( sender.tab.id ) //turn on the icon if needed
-        RequestForAction( sender.tab.id, true )
+        tabs.showPageIcon( sender.tab.id )
+        //tabs.needUserAction( sender.tab.id, true ) //DEBUG
         
         if( !!message.request && !!message.request.type && message.request.data ) {
             if( message.request.type == "sendAsync" ) {
@@ -85,4 +44,13 @@ chrome.runtime.onMessage.addListener( function(message, sender, sendResponse ) {
     }
     
     sendResponse( {} )
+});
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+    tabs.deleteTab( tabId ) 
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    tabs.deleteTab( tabId )
 });
