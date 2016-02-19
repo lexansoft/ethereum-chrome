d086c2faf36447d9e2fdda359698eedf80ce6aa2
alexn <alexn@lexansoft.com>
2016-02-19 12:00:42 -0800

    no message

--- start_geth.bash
+++ start_geth.bash
@@ -11,5 +11,8 @@ case $response in [nN][oO]|[nN])
         ;;
 esac
 
-geth --rpc --rpccorsdomain "*"  --unlock ${1:-""} --rpcapi "db,eth,net,personal,web3"  --autodag --rpcaddr "0.0.0.0"  --verbosity "0" --etherbase 0xb1f9faafe5f917d92ebf5b21c25dc77c2cf86000    console 2>>  /Users/alexn/geth.log 
+geth --rpc --rpcapi "db,eth,net,personal,web3" 
+
+
+# geth --rpc --rpccorsdomain "*"  --unlock ${1:-""} --rpcapi "db,eth,net,personal,web3"  --autodag --rpcaddr "0.0.0.0"  --verbosity "0" --etherbase 0xb1f9faafe5f917d92ebf5b21c25dc77c2cf86000    console 2>>  /Users/alexn/geth.log 
 
